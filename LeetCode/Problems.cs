54d851c0edf05cd67ac32e63253ec3eea11a5858
Alexandru Pupsa <alexandru.pupsa@tremend.com>
2023-03-28 12:57:50 +0300

    Add project files.

---
+++ LeetCode/Problems.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode
+{
+    public static class Problems
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            var output = new int[nums.Length];
+            var sum = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                output[i] = sum;
+            }
+            return output;
+        }
+
+        public static int PivotIndex(int[] nums)
+        {
+            var left = 0;
+            var right = nums.Sum();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                right -= nums[i];
+                if (left == right)
+                {
+                    return i;
+                }
+                left += nums[i];
+            }
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,7 @@
+﻿using LeetCode;
+
+//var r1 = Problems.RunningSum(new[] { 1, 2, 3, 4 });
+//Console.WriteLine(string.Join(",", r1));
+
+var r2 = Problems.PivotIndex(new[] { 2, 1, -1 });
+Console.WriteLine(r2);
